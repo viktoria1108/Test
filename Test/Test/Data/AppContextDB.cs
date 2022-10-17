e48d4b3afb9b25acfa5a8248484f0165cf52a451
Vik_toria <72942985+viktoria1108@users.noreply.github.com>
2022-10-17 16:11:40 +0300

    Models

--- Test/Test/Data/AppContextDB.cs
+++ Test/Test/Data/AppContextDB.cs
@@ -1,25 +1,6 @@
 ﻿namespace Test.Data
 {
-    public class AppContextDB : IdentityDbContext<User>
+    public class AppContextDB
     {
-        public AppContextDB(DbContextOptions<AppContextDB> options) : base(options) { }
-
-        public DbSet<Accommodation> Accommodations { get; set; }
-
-        public DbSet<Offer> Offers { get; set; }
-
-        public DbSet<Address> Address { get; set; }
-
-        public DbSet<Booking> Booking { get; set; }
-
-        public DbSet<Picture> Pictures { get; set; }
-
-        public DbSet<Room> Rooms { get; set; }
-
-        public DbSet<Amenity> Amenity { get; set; }
-
-        public DbSet<Bookmark> Bookmark { get; set; }
-
-        public DbSet<Transaction> Transactions { get; set; }
     }
 }
